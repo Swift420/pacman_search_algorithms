094606911099ccf4c43200c20a5a9f413585a8d4
Swift420 <apollosdavid95@gmail.com>
2023-04-17 14:20:19 +0200

    import cmath

--- p2/multiAgents.py
+++ p2/multiAgents.py
@@ -11,6 +11,7 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
+from cmath import inf
 
 from util import manhattanDistance
 from game import Directions
